3c19d864c4c9982dad1b8790c4bd1c6fa4d4e538
zmkarakas <50174304+zmkarakas@users.noreply.github.com>
2020-05-22 17:22:22 +0200

    Update Hangman game.py

--- Hangman game.py
+++ Hangman game.py
@@ -1,3 +1,50 @@
+def getAvailableLetters(lettersGuessed):
+    '''
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters that represents what letters have not
+      yet been guessed.
+    '''
+    import string
+    
+    SET_ALL_LETTERS = set(string.ascii_lowercase)
+    answer = SET_ALL_LETTERS - set(lettersGuessed)
+    answer = "".join(sorted(list(answer)))
+    
+    return answer
+
+def getGuessedWord(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secretWord have been guessed so far.
+    '''
+    result = ""
+    for char in secretWord:
+        if char in lettersGuessed:
+            result += char
+        else:
+            result += "_"
+    return result
+
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+ 
+    #for char in secretWord:
+      #  if char not in lettersGuessed:
+     #       return False
+    #return True
+    
+    set_SW = set(secretWord)
+    set_LG  = set(lettersGuessed)
+    answer = True if set_SW.issubset(set_LG) else False 
+    return answer 
+
 def hangman(secretWord):
     '''
     secretWord: string, the secret word to guess.
